c94170a6c5eda394b8e2779ee5db30e2912d3cac
Mateus Silva <dmateusmsilva@gmail.com>
2021-06-10 13:23:25 -0300

    commit projeto media

---
+++ frmMedia.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace media
+{
+    partial class frmMedia
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMedia));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.lblTrabalho = new System.Windows.Forms.Label();
+            this.txtTrabalho = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.gpbResultado = new System.Windows.Forms.GroupBox();
+            this.gpbResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblTitulo.Location = new System.Drawing.Point(325, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(295, 42);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cálculo de Média";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota1.ForeColor = System.Drawing.Color.Gold;
+            this.lblNota1.Location = new System.Drawing.Point(68, 150);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(95, 26);
+            this.lblNota1.TabIndex = 1;
+            this.lblNota1.Text = "1ª Nota:";
+            this.lblNota1.Click += new System.EventHandler(this.lblNota1_Click);
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota2.ForeColor = System.Drawing.Color.Gold;
+            this.lblNota2.Location = new System.Drawing.Point(249, 150);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(95, 26);
+            this.lblNota2.TabIndex = 1;
+            this.lblNota2.Text = "2ª Nota:";
+            this.lblNota2.Click += new System.EventHandler(this.lblNota2_Click);
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota3.ForeColor = System.Drawing.Color.Gold;
+            this.lblNota3.Location = new System.Drawing.Point(68, 235);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(95, 26);
+            this.lblNota3.TabIndex = 1;
+            this.lblNota3.Text = "3ª Nota:";
+            this.lblNota3.Click += new System.EventHandler(this.lblNota3_Click);
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota1.Location = new System.Drawing.Point(73, 179);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 26);
+            this.txtNota1.TabIndex = 0;
+            this.txtNota1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota2.Location = new System.Drawing.Point(254, 179);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 26);
+            this.txtNota2.TabIndex = 1;
+            this.txtNota2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota3.Location = new System.Drawing.Point(73, 264);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 26);
+            this.txtNota3.TabIndex = 2;
+            this.txtNota3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTrabalho
+            // 
+            this.lblTrabalho.AutoSize = true;
+            this.lblTrabalho.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrabalho.ForeColor = System.Drawing.Color.Gold;
+            this.lblTrabalho.Location = new System.Drawing.Point(249, 235);
+            this.lblTrabalho.Name = "lblTrabalho";
+            this.lblTrabalho.Size = new System.Drawing.Size(108, 26);
+            this.lblTrabalho.TabIndex = 1;
+            this.lblTrabalho.Text = "Trabalho:";
+            this.lblTrabalho.Click += new System.EventHandler(this.lblNota1_Click);
+            // 
+            // txtTrabalho
+            // 
+            this.txtTrabalho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTrabalho.Location = new System.Drawing.Point(254, 264);
+            this.txtTrabalho.Name = "txtTrabalho";
+            this.txtTrabalho.Size = new System.Drawing.Size(100, 26);
+            this.txtTrabalho.TabIndex = 3;
+            this.txtTrabalho.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCalcular.Location = new System.Drawing.Point(70, 333);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(103, 46);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLimpar.Location = new System.Drawing.Point(254, 333);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(103, 46);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(120, 481);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 29);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(93, 103);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(156, 80);
+            this.txtResultado.TabIndex = 4;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gpbResultado
+            // 
+            this.gpbResultado.Controls.Add(this.txtResultado);
+            this.gpbResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbResultado.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.gpbResultado.Location = new System.Drawing.Point(545, 131);
+            this.gpbResultado.Name = "gpbResultado";
+            this.gpbResultado.Size = new System.Drawing.Size(327, 280);
+            this.gpbResultado.TabIndex = 6;
+            this.gpbResultado.TabStop = false;
+            this.gpbResultado.Text = "Resultado";
+            // 
+            // frmMedia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(974, 637);
+            this.Controls.Add(this.gpbResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtTrabalho);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblTrabalho);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMedia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Média";
+            this.Load += new System.EventHandler(this.frmMedia_Load);
+            this.gpbResultado.ResumeLayout(false);
+            this.gpbResultado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label lblTrabalho;
+        private System.Windows.Forms.TextBox txtTrabalho;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.GroupBox gpbResultado;
+    }
+}
+
---
+++ frmMedia.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace media
+{
+    public partial class frmMedia : Form
+    {
+        public frmMedia()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMedia_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNota2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNota3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNota1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtNota1.Text == "")
+            {
+                MessageBox.Show("Insira as notas do aluno nas caixas de textos respectivas para prosseguir", "opa",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                double resultado, numero1, numero2, numero3, numerot;
+
+                numero1 = double.Parse(txtNota1.Text);
+                numero2 = double.Parse(txtNota2.Text);
+                numero3 = double.Parse(txtNota3.Text);
+                numerot = double.Parse(txtTrabalho.Text);
+
+                resultado = (numero1 + numero2 + numero3 + numerot) / 4;
+                txtResultado.Text += resultado;
+                
+                if (resultado >= 6)
+                {
+                    txtResultado.ForeColor = Color.Lime;
+                    lblResultado.ForeColor = Color.Lime;
+                    lblResultado.Text = "O aluno conseguiu alcançar a média " + resultado + " e foi aprovado!";
+                }
+                else
+                {
+                    txtResultado.ForeColor = Color.Maroon;
+                    lblResultado.ForeColor = Color.Maroon;
+                    lblResultado.Text = "O aluno conseguiu alcançar a média " + resultado + " e acabou reprovado...";
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            txtNota3.Text = "";
+            txtTrabalho.Text = "";
+            txtResultado.Text = "";
+            lblResultado.Text = "";
+        }
+    }
+}
